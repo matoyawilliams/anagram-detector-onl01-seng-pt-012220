b23b36cc082eb8ab5102acbf79cdc36a05d2b735
Matoya Williams <matoyawilliams@yahoo.com>
2020-02-20 23:34:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,3 +7,14 @@ class Anagram
   end
   
   def match(array_of_words)
+ array_of_words.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+    end
+  end
+  
+  
+end
+
+listen = Anagram.new("listen")
+#listen.match(%w(enlists))
+listen.match(%w(enlists google inlets banana))
